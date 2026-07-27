db9af46b177c31666d6a2dc06003f9c067e2bc30
BotrousSarofim <sarofimpeter@gmail.com>
2026-07-27 16:50:47 +0300

    Just Finished Student  Task5 and Finished All Tasks

--- Student.dart
+++ Student.dart
@@ -2,7 +2,7 @@ class Student {
   String name;
   String grade;
 
-  // Constructor with optional positional parameter [this.grade]
+  // Constructor with optional parameter  
   Student(this.name,   [this.grade = "Not Graded"] );
 }
 
@@ -14,7 +14,7 @@ void main() {
   Student student2 = Student("Samy");
 
   // Print Students
-  
+
   print("Student 1: ${student1.name}, Grade: ${student1.grade}");
   print("Student 2: ${student2.name}, Grade: ${student2.grade}");
 }
